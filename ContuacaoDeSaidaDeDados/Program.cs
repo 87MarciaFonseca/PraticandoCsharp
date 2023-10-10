f4a3be5e3788f09eeceeba24b1c0b7e6bb512225
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-10-10 15:48:41 -0300

    Ultilização de espaces em strings

---
+++ ContuacaoDeSaidaDeDados/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Specialized;
+
+Console.WriteLine( "Continuaçao de Saída de Dados: Usando sequencia de escapes\n");
+Console.WriteLine( );
+
+string local = "c: \\dados\\poesias.txt";
+string frase = "Ele falou:\"Te amo pandinha \"";
+
+string pizza = "\nPizza\nde\tChocolate";
+
+string bolo = "\nBolo\tde\tChocolate";
+
+Console.WriteLine( local);
+Console.WriteLine(frase );
+Console.WriteLine(pizza );
+Console.WriteLine(bolo );
+
+Console.ReadLine( );
