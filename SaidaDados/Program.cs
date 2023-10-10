5495afaa5f1924108763b3a2d156d82dd37ea464
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-10-09 21:44:53 -0300

    aplicações de concatenação,interpolaão e palce horlders

--- SaidaDados/Program.cs
+++ SaidaDados/Program.cs
@@ -1,11 +1,13 @@
-﻿Console.WriteLine(   "Saída de Dados:Formatação");
+﻿using System;
+
+Console.WriteLine(   "Saída de Dados:Formatação");
 Console.WriteLine( );
 
 int idade = 25;
 string nome = "Maria";
 
 Console.WriteLine(nome);
-Console.WriteLine(idade);
+Console.Write(idade);
 
 
 
@@ -15,9 +17,20 @@ Console.Write( nome);
 Console.Write(" tem ");
 Console.Write( idade);
 Console.Write(" anos " );
+Console.WriteLine( );
+Console.WriteLine(" ------------Concatenação-------------- " );
+
 
 //usar a concatenação: usando o operador +
-Console.WriteLine(nome + " tem" + idade + " anos ");
+Console.WriteLine(nome + " tem " + idade + " anos ");
+Console.Write(nome + " tem " + idade + " anos ");//ambos console imprimir da mesma forma
+Console.WriteLine();
+Console.WriteLine("----------Interpolação------------------");
+//usar a interpolação de strings: $ -> a interpolação {}
+Console.WriteLine($"{nome} tem {idade}  anos ");
+Console.WriteLine( "--------------------place holders---------------------");
+//uasr place holders: usar {} com numeração com inicio em zero
+Console.WriteLine("{0} tem {1}  anos ",nome ,idade);
 
 
 Console.ReadLine();
