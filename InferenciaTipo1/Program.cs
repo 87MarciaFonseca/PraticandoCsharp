72bf649c87bff5a3fb5ea365b898604c1bd7f8f6
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-10-21 22:28:24 -0300

    Inferências de tipos var,com exemplos práticos .É usado em laços for ,foreach e tipos anônimos.

---
+++ InferenciaTipo1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine( "Inferência Tipos (var) \n");
+
+var idade = 25; // uso do var identificar o tipo da variável
+var nome = "Urso";
+var salario = 2500m;
+Console.WriteLine( $"{nome} tem {idade} e ganha {salario.ToString("c")}");
+
+//var limitações
+
+var salario = null;// não pode atribuir um valor null
+var titulo;//declarar não inicializar
+var salario, imposto, total//não pode multideclarações 
+
+//não posso mudar o tipo após inicializar
+
+var num = 10;
+num =;
+num = num + 20;
+num = " Teste";
+
+var teste = new Teste();//estanciar é criar um objeto
+Console.ReadLine( );
+
+class Teste {
+
+    public void MeuMetodo() {
+
+        Console.WriteLine( "Meu Método");
+    }
+
+}
+//var é considerado uma forma inferir tipos,identifica sem precisar 
+//escrever o tipo,seu user deve ser moderado. 
+//usados para declarar tipos anônimos,laços for e foreach
+// e  instruções using
