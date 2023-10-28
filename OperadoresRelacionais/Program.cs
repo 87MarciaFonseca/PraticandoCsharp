60889ed82ddf7f69a5c83b64b2eb9eeb74eacbb7
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-10-27 23:40:12 -0300

    Operadores relacionais,suas aplicações com exemplos em string,int ,bool e Equals.

---
+++ OperadoresRelacionais/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine( " Operadores relacionais");
+
+
+Console.WriteLine("----exemplo usando int------")
+/*
+int x = 10;
+int y = 20;
+
+Console.WriteLine( x == y);// aqui vai ser false
+
+
+int x = 10;
+int y = 20;
+bool resultado = x == y;// esse métdo btm vai ser false
+
+ConsoleWriteLine("------exemplo usando string-----")
+//operador relacional de igualdade(==) com string
+string a = "curso";
+string b = "Curso";
+Console.WriteLine( a == b); //false
+Console.WriteLine( a.Equals(b));//false,tem mesmo conportamento do operador de igualdade
+
+string a = "curso";
+string b = "Curso";
+
+bool resultado = a == b;
+Console.WriteLine( resultado);*/
+
+
+
+
+/*Console.WriteLine( "--------exemplo usando bool e sinais de igualdade-------");
+int x = 10;
+int y = 20;
+
+Console.WriteLine($"Valor de x {x} ");
+Console.WriteLine($"Valor de y {y}");
+
+Console.WriteLine(x == y);
+Console.WriteLine(x > y);
+Console.WriteLine(x < y);
+Console.WriteLine(x >= y);
+Console.WriteLine(x <= y);
+Console.WriteLine(x != y); //tool /opção/fonte color /cascadia code,para alterar a fonte
+
+Console.WriteLine(x == y);
+bool resultado = x == y;
+Console.WriteLine( resultado);
+
+*/
+
+
+Console.WriteLine("-----exemplo usando string para o Equals----");
+string a = "curso";
+string b = "Curso";
+
+Console.WriteLine( a == b );// ambos tem o memso resultado na saída
+Console.WriteLine( a.Equals(b));// ambos tem o memso resultado na saída
+Console.ReadLine( );
