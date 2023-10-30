5caafe3ff8d4fcf33c1ae13f1e771cbe49adba04
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-10-30 15:30:39 -0300

    Tipos de referência Anuláveis,utilizando operador ?,para corrigir erro de alerta do compilador.

---
+++ NullabeReferenceTypes/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine( " Tipos de referência Anuláveis \n");
+
+string? nome = null;//sem operador ? ,emitido uma alerta do compilador
+
+Console.WriteLine( nome?.ToUpper());//utilizar operador null'?'p/eveitar o erro
+
+
+Console.ReadKey( );
+
+
