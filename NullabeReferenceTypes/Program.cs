162144f8598319b75ccb1296a1e0436fd5413801
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-10-30 15:39:23 -0300

    Completação da aula de tipos de referência anuláveis.

--- NullabeReferenceTypes/Program.cs
+++ NullabeReferenceTypes/Program.cs
@@ -1,9 +1,12 @@
 ﻿Console.WriteLine( " Tipos de referência Anuláveis \n");
 
-string? nome = null;//sem operador ? ,emitido uma alerta do compilador
+string? nome1 = null;//sem operador ? ,emitido uma alerta do compilador
+string? nome2 = "";
 
-Console.WriteLine( nome?.ToUpper());//utilizar operador null'?'p/eveitar o erro
+Console.WriteLine( nome1?.ToUpper());//utilizar operador null'?' e ToUpper p/eveitar o erro
 
+Console.WriteLine(nome1?.ToUpper());//ambas expressões vão dá o mesmo resultado
+//null,devido atulização dos operadores.
 
 Console.ReadKey( );
 
