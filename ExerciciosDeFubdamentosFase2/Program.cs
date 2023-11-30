13ec54e83a93594d6fc97677ef688c7c0bb56f79
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-11-30 17:18:31 -0300

    Exercicos da fase2

--- ExerciciosDeFubdamentosFase2/Program.cs
+++ ExerciciosDeFubdamentosFase2/Program.cs
@@ -105,6 +105,30 @@ var resultado = (nome == "admin"|| nome == " Maria ") && senha == 123? "login  l
 
 Console.WriteLine( resultado);
 
+//Exercicio 10
+
+( v) string? nome; é um exemplo de nullable reference type;
+(v ) para x igual a 0 , Console.WriteLine(x++); imprime o valor 1
+( f) A ordem de precedência dos operadores lógicos é : ! , || e && (NOT, OR e AND)
+(v ) para y igual a 0 , Console.WriteLine(++y); imprime o valor 1
+(v ) (10 % 2 == 0) ? “Par” : “Impar”; Vai retornar “Impar”
+(v ) Para x = 25 e y=5 , a expressão ( y >= x ) && (y <= x); retorna true;
+( ) Os nullable reference types emitem um alerta em tempo de compilação para uso do null
+( ) Para x = 10, o código Console.WriteLine(x+=x); imprime o valor 10;
+( ) Para x = 5, a expressão !(9 >= x) && x <= 7 || x == 5); retorna false;
+( ) O operador ?. permite verificar se um objeto é null e evitar o NullReferenceException
+( ) Qualquer tipo de dado pode ser declarado como anulável com a ajuda do operador "?( ) string? nome; é um exemplo de nullable reference type;
+( ) para x igual a 0 , Console.WriteLine(x++); imprime o valor 1
+( ) A ordem de precedência dos operadores lógicos é : ! , || e && (NOT, OR e AND)
+( ) para y igual a 0 , Console.WriteLine(++y); imprime o valor 1
+( ) (10 % 2 == 0) ? “Par” : “Impar”; Vai retornar “Impar”
+( ) Para x = 25 e y=5 , a expressão ( y >= x ) && (y <= x); retorna true;
+( ) Os nullable reference types emitem um alerta em tempo de compilação para uso do null
+( ) Para x = 10, o código Console.WriteLine(x+=x); imprime o valor 10;
+( ) Para x = 5, a expressão !(9 >= x) && x <= 7 || x == 5); retorna false;
+( ) O operador ?. permite verificar se um objeto é null e evitar o NullReferenceException
+( ) Qualquer tipo de dado pode ser declarado como anulável com a ajuda do operador "?
+
 
 Console.ReadKey( );
 
