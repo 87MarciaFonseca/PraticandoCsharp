013afc2d455fbe66deaba71dc471513f34a5e9f1
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-12-15 17:41:28 -0300

    Estruturas If,exemplos de aplicações.

---
+++ InstrucaoIf/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Console.WriteLine("Instrução If");
+//Exemplo: Cliente especial tem desconto
+
+/*Console.WriteLine( "Cliente Especial (true/false) \t");
+var resposta = Convert.ToBoolean(Console.ReadLine());
+
+if (resposta) // converter para boolean tirando == "S" e deixando só resposta
+
+    Console.WriteLine( "Desconto de 10%");
+
+
+Console.WriteLine( "Fim do processamento");
+
+Console.WriteLine( "Cliente Especial (true/false)\t");
+bool clienteEspecial = Convert.ToBoolean(Console.ReadLine());
+
+if (clienteEspecial)
+{
+    Console.WriteLine("Desconto de 10%");
+}
+*/
+
+//Exemplo: Comparar se x e maior ,menor ou igual a y
+Console.WriteLine( "Informe o valor de x \t");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine( "\n Informe o valor de y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y) {
+
+    Console.WriteLine("x é maior que y");
+}
+if (x< y) {
+
+    Console.WriteLine("x é maior que y");
+        
+   }
+
+Console.ReadKey( );
