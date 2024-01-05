871d0c13c77b4a557796187d285a76b50cffcb62
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-01-05 16:54:15 -0300

    Instrução If -else If,com aplicações em exemplos.

---
+++ IfElseIf/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Xml;
+
+Console.WriteLine("Instrução if -else-if");
+
+if (nota < 5)
+{
+
+    Console.WriteLine("ALUNO REPROVADO");
+}
+else if (nota >= 5 && XmlNotation < 6)
+{
+    Console.WriteLine("ALUNO EM RECUPERAÇÃO");
+}
+else if (nota >= 6 && nota <= 9) {
+
+
+}
+
+
+Console.ReadKey( );
