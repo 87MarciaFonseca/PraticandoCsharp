5da823a5161af5f173bb281f1fa6fb92c9654f7f
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-01-15 15:45:32 -0300

    Instrução IF-ELSE-IF,com exemplos práticos .

--- IfElseIf/Program.cs
+++ IfElseIf/Program.cs
@@ -1,1 +1,29 @@
 ﻿
+
+Console.WriteLine("Informe a nota do aluno\t");
+var nota = Convert.ToDouble(Console.ReadLine());
+
+
+if (nota < 5)
+{
+
+    Console.WriteLine("\nAluno Reprovado");
+}
+else if (nota >= 5 && nota < 6)
+{
+    Console.WriteLine("\n Aluno em Recuperação");
+}
+else if (nota >= 6 && nota <= 9)
+{
+    Console.WriteLine("\nAluno Aprovado");
+
+}
+else 
+{
+
+    Console.WriteLine("\nAluno Aprovado com Distinção");
+}
+
+Console.WriteLine("\nFim do Processamento...");
+
+Console.ReadKey();
