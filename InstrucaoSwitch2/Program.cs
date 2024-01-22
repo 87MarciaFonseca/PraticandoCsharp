52037c2c6adba91d0ee3835568198c99ea12d1bf
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-01-22 12:23:18 -0300

    Instrução switch 2,com exemplos práticos.

---
+++ InstrucaoSwitch2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Instrução Switch 2");
+
+//exemplo - Dias dos meses do ano (2022)
+
+var mes = Console.ReadLine().ToLower();//comando para converter em minuscula
+
+switch (mes)
+{
+     case "janeiro":
+     case "março":
+     case "maio":
+     case "julho":
+     case "agosto":
+     case "outubro":
+     case "dezembro":
+        Console.WriteLine( "Este mês tem 31 dias");
+        break;
+
+     case "fevereiro":
+        Console.WriteLine( "Este mês tem 28 ou 29 dias");
+        break;
+
+    default:
+
+        Console.WriteLine("Este mês tem 30 dias");
+        break;
+
+
+}
+Console.WriteLine("\n Fim do processamento....";
+Console.ReadKey( );
