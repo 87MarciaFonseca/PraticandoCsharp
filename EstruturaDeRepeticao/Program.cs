8df757e77514bb3e12efe850c2c960af4fe97898
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-01-26 12:51:56 -0300

    Estrutura de repetição com goto/label

---
+++ EstruturaDeRepeticao/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Estrutura de repetição: Instrução goto e label");
+//Esse não é mais usado,esse exemplo para apenas fins de contextualização
+
+int i = 1;
+
+repetir:// esse comando identifica uma label
+
+Console.WriteLine($"i = {i}");
+
+i++;
+
+if ( i <= 10 )
+    goto repetir;//comando para repetir,esse processo é laço loop
+
+Console.WriteLine( "Fim do processamento..");
+
+Console.ReadKey( );
