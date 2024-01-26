11edebf2c4b2e7f4986a583a685dac1534c584e2
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-01-26 11:45:29 -0300

    Instrução swtch 2,aplicaçao de switch aninhadas.

--- InstrucaoSwitch2/Program.cs
+++ InstrucaoSwitch2/Program.cs
@@ -1,8 +1,9 @@
-﻿Console.WriteLine("Instrução Switch 2");
+﻿/*Console.WriteLine("Instrução Switch 2");
 
 //exemplo - Dias dos meses do ano (2022)
-
-var mes = Console.ReadLine().ToLower();//comando para converter em minuscula
+//comando para retornar uma string em minuscula.
+Console.WriteLine("Informe o nme do mês\t");
+var mes = Console.ReadLine().ToLower();
 
 switch (mes)
 {
@@ -13,11 +14,11 @@ switch (mes)
      case "agosto":
      case "outubro":
      case "dezembro":
-        Console.WriteLine( "Este mês tem 31 dias");
+        Console.WriteLine("Este mês tem 31 dias");
         break;
 
      case "fevereiro":
-        Console.WriteLine( "Este mês tem 28 ou 29 dias");
+        Console.WriteLine("Este mês tem 28 ou 29 dias");
         break;
 
     default:
@@ -27,5 +28,47 @@ switch (mes)
 
 
 }
-Console.WriteLine("\n Fim do processamento....";
+Console.WriteLine("\n Fim do processamento....");
+Console.ReadKey( );*/
+
+//Exemplo:Instrução switch - aninhadas
+Console.WriteLine( " Estrutura Switch-case \n");
+
+int cargo = 0;
+int funcao = 0;
+Console.WriteLine("Você é Gerente(1) ou Programador(2) \t");
+cargo = Convert.ToInt32(Console.ReadLine());
+
+if (cargo == 2) 
+{
+    Console.WriteLine( "Você é Junior(1) ou Senior(2)");
+    funcao = Convert.ToInt32(Console.ReadLine());
+
+}
+switch (cargo) 
+{ 
+ case 1:
+        Console.WriteLine( "\nBem Vindo Gerente");
+        break;
+ case 2:
+        Console.WriteLine( "\nBem - Vindo Programador");
+        switch (funcao) 
+        { 
+         case 1:
+                Console.WriteLine( "Você é Junior ");
+                break;
+         case 2:
+                Console.WriteLine( "\nVocê é Senior");
+                break;
+        default:
+                Console.WriteLine( "\nFunção desconhecida");
+                break;
+        }
+        break;
+    default:
+        Console.WriteLine( "Função desconhecida");
+        break;
+}
+
+Console.WriteLine( "\n Fim do processamento...");
 Console.ReadKey( );
