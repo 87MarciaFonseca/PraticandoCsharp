67c46908d3dd3066277ce9e75d3a032ae0a005ae
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-01-27 19:27:37 -0300

    Estrutura de repetição while usando incremento e decremento e imprimindo uma tabuada.

--- InstrucaoWhile1/Program.cs
+++ InstrucaoWhile1/Program.cs
@@ -1,2 +1,55 @@
 ﻿
+using System.Diagnostics.SymbolStore;
+using System.Text.Encodings.Web;
+
 Console.WriteLine("Instrução While");
+
+//Exemplo usando método de incremento 
+
+var i = 1;
+while (i < 10)//condição do laço while
+{
+    Console.WriteLine($"i = {i}");
+    i++;// tem incrementar ou descrementar para evitar loop infinito
+
+}
+
+
+
+//Exemplo usando método de descremento
+
+var i = 10;
+while (i > 0)//condição while 
+{
+    Console.WriteLine($"i = {i}");
+    i--;//usando o decremento p/ evitar loop infinito
+
+}
+
+
+//Exemplo:Imprimir a tabuada de um número maior que zero
+int numero;
+int contador = 1;
+Console.WriteLine( "\nDigite um numero maior que zero\t");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0) 
+{
+    //loop while
+    Console.WriteLine( $"\n Tabuada do {numero}\n");
+    while(contador < 11)
+    {
+
+        Console.WriteLine($"{numero}x {contador} = {numero * contador}");
+        contador++;
+  
+    }
+}
+else
+{
+
+    Console.WriteLine("\nNúmero deve ser maior que zero !!");
+}
+ 
+Console.WriteLine("\nFim do processsamento..");
+Console.ReadKey();
