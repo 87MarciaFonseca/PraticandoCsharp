13c1b8bc3fce492950424a493a0cd76b829b2397
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-01-30 12:19:15 -0300

    Aplicações com uso do do-while e do-wile aninhadas.

--- InstrucaoDowhile/Program.cs
+++ InstrucaoDowhile/Program.cs
@@ -1,1 +1,52 @@
 ﻿Console.WriteLine("\n Instrução do-while");
+
+//Imprimir o valor de uma variável i de 1 a 10.
+var i = 1;
+do
+{
+    Console.WriteLine($"i = {i}");
+    i++;
+
+}
+while (i <= 10);//Enquanto i for menor ou igual a 10 as instruções serão executadas.
+                //Cuidado com o laço ou loop infinito!!
+
+
+//Imprimir o valor de uma variável i de 1 a 10 e sair do loop quando
+//i for maior que 7
+
+var i = 1;
+do
+{
+    Console.WriteLine($"i = {i}");
+    i++;
+
+    if (i > 7)
+        break;
+}
+while (i <= 10);
+
+//Instrução do-while aninhadas: Exibir coordenadas do plano(x,y)
+
+int x = 0;
+do 
+{
+    int y = 0;
+    do
+    {
+        Console.Write($"({x},{y}) ");//Primeiro laço do- while
+        y++;
+
+    }
+    while (y < 5);//Segundo laço do- while
+    y++;
+    x++;
+    Console.WriteLine( );
+}
+while (x < 5);
+
+
+
+
+Console.WriteLine("\nFim do processamento");
+Console.ReadKey( );
