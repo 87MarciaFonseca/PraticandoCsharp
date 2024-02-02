af06a6d6d7cb504bb18a406d567f72789b29163d
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-02-01 22:52:19 -0300

    Instrução For ,loop For e a estrutura para criar um laço.

--- InstrucaoFor1/Program.cs
+++ InstrucaoFor1/Program.cs
@@ -1,29 +1,36 @@
 ﻿Console.WriteLine("Instrução For");
 
-//Loop for
-/*for (int i = 0, j =0; i + j <= 20; i++, j++) 
+//Loop for - estrutura :
+//for (inicialização;condição;iteração)
+//{
+//instruções
+//
+//}
+
+//Loop For: Aplicação usando número inteiro
+for (int i = 0, j = 0; i + j <= 20; i++, j++)
 {
-    Console.WriteLine( $"i ={i} e j={j}");
-    
-}*/
+    Console.WriteLine($"i ={i} e j={j}");
+
+}
 
 
-/*for (int x = 0; x< 5; x++) 
+for (int x = 0; x < 5; x++)
 {
 
-    for (int y = 0; y < 5; y++) 
+    for (int y = 0; y < 5; y++)
     {
 
         Console.Write($"({x},{y})  ");
     }
-    Console.WriteLine();*/
-/*}
-*/
+    Console.WriteLine();
+}
+
 
 //Programa c# usando laço for para exibir a tabuada de multiplicação 
 //de número  doubles,que podem ser usados int também.
 
-/*double resultado, numero;
+double resultado, numero;
 double i;
 
 Console.Write(" Informe o número inteiro maior que zero:\t");
@@ -31,16 +38,16 @@ numero = Convert.ToInt32(Console.ReadLine());
 
 
 
-    for (i = 1.01; i <= 10.10; i += 0.1) //for (var i = 1; i<= 10; i++) p/ inteiros
-    {
-        resultado = numero * i;
-        //Console.WhriteLine(numero + "x" + i + "=" + resultado)
-        Console.WriteLine(numero + "x" + i + " = " + resultado);
-    }
+for (i = 1.01; i <= 10.10; i += 0.1) //for (var i = 1; i<= 10; i++) p/ inteiros
+{
+    resultado = numero * i;
+    //Console.WhriteLine(numero + "x" + i + "=" + resultado)
+    Console.WriteLine(numero + "x" + i + " = " + resultado);
+}
 
 
 
-*/
+
 
 // O mesmo código,agora usando if para válida um numero.
 
@@ -72,5 +79,4 @@ else
 
 
 Console.WriteLine("\nFim do processamento...");
-Console.ReadKey();
 Console.ReadKey();
