c4b59d81dd7662991e07d2aed2a1972181fa0ffd
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-02-05 23:35:04 -0300

    Loop for aninhado,com aplição prática

--- InstrucaoFor2/Program.cs
+++ InstrucaoFor2/Program.cs
@@ -22,13 +22,29 @@ for (; ; )
     i++;
 }*/
 
-//Multiplas interações em um loop for.
+/*//Multiplas interações em um loop for.
 
 for (int i = 0,j = 0; i +j <= 20; i++,j++)
 {
     Console.WriteLine( $"i = {i} e j ={j}");
 
 }
+*/
+
+//Loop For aninhado
+for (int x= 0; x<5; x++) 
+{
+    for (int y = 0; y < 5; y++) 
+    {
+        Console.Write($"{x},{y}  ");
+
+
+    }
+    Console.WriteLine();
+}
+
+
+
 
 
 Console.WriteLine( "\nFim do processamento");
