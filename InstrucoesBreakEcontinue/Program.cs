1a8c6ce178115916a55e6d81750023b1d6665b56
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-02-07 00:14:31 -0300

    Intruções break e continue,novas aplicações ,usando while e break e continue.

--- InstrucoesBreakEcontinue/Program.cs
+++ InstrucoesBreakEcontinue/Program.cs
@@ -5,23 +5,23 @@ using System.Threading.Channels;
 Console.WriteLine("Instruções break e Continue");
 
 //Instrução break:
-/*for (; ; ) 
+for (; ; )
 {
     Console.WriteLine("Tecle algo() x sai");
     var opcao = Console.ReadLine();
     Console.WriteLine(opcao.ToUpper());
 
-    if(opcao == "x" || opcao == "x") 
+    if (opcao == "x" || opcao == "x")
     {
         Console.WriteLine("Tchau...");
         break;
 
     }
 
-}*/
+}
 
 //Instrução continue:
-for (int i = 1; i <= 10; i++) 
+for (int i = 1; i <= 10; i++)
 {
     if (i == 4)
         continue;
@@ -29,5 +29,34 @@ for (int i = 1; i <= 10; i++)
     Console.WriteLine(i);
 
 }
+
+//Loop While:
+int n = 1;
+while (n <= 10)
+{
+    if (n == 5)
+    {
+
+        n++;
+        continue;// sai do loop while
+
+    }
+    Console.WriteLine($"n={n}");
+    n++;
+}
+
+//Usando break e continue:
+for (int i = 1; i < 10; i++) 
+{
+    if (i == 5)
+        continue;
+
+    if (i == 8)
+        break;
+
+    Console.WriteLine(i);
+
+}
+
 Console.WriteLine("\nFim do processamento...");
 Console.ReadKey();
