d52e5c4d2fee972e5a6b4026ab3a2ca18ea5359a
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-02-07 00:03:37 -0300

    Instruções break e contunue,com aplicações de ambas.

---
+++ InstrucoesBreakEcontinue/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+
+Console.WriteLine("Instruções break e Continue");
+
+//Instrução break:
+/*for (; ; ) 
+{
+    Console.WriteLine("Tecle algo() x sai");
+    var opcao = Console.ReadLine();
+    Console.WriteLine(opcao.ToUpper());
+
+    if(opcao == "x" || opcao == "x") 
+    {
+        Console.WriteLine("Tchau...");
+        break;
+
+    }
+
+}*/
+
+//Instrução continue:
+for (int i = 1; i <= 10; i++) 
+{
+    if (i == 4)
+        continue;
+
+    Console.WriteLine(i);
+
+}
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
