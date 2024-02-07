0d030b6fe14ee0e816d2d2bfd0f3dc17c5add71f
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2024-02-07 13:36:47 -0300

    Novas alterações para inserir novas aplicações .

--- InstrucoesBreakEcontinue/Program.cs
+++ InstrucoesBreakEcontinue/Program.cs
@@ -4,40 +4,40 @@ using System.Threading.Channels;
 
 Console.WriteLine("Instruções break e Continue");
 
-//Instrução break:
-for (; ; )
+/*//Instrução break:
+for (; ; )//laço for infinito
 {
-    Console.WriteLine("Tecle algo() x sai");
-    var opcao = Console.ReadLine();
-    Console.WriteLine(opcao.ToUpper());
+    Console.WriteLine("Tecle algo (x sai)");
+    var opcao = Console.ReadLine();// receber informação via teclado
+    Console.WriteLine(opcao.ToUpper());//transforma o texto  p/ caixa alta
 
-    if (opcao == "x" || opcao == "x")
+    if (opcao == "X" || opcao == "x")//condição
     {
         Console.WriteLine("Tchau...");
-        break;
+        break;// comnando p/ sair sair do loop
 
     }
 
-}
+}*/
 
-//Instrução continue:
-for (int i = 1; i <= 10; i++)
+/*//Instrução continue:
+for *//*(int i = 1; i <= 10; i++)
 {
-    if (i == 4)
-        continue;
+    if (i == 4)//condição
+        continue;// comando p/ sair do loop
 
     Console.WriteLine(i);
 
-}
+}*/
 
 //Loop While:
 int n = 1;
 while (n <= 10)
 {
-    if (n == 5)
+    if (n == 5)// se operação não for verdade,ele vai continuar o loop até 4 e depois incrementa
     {
 
-        n++;
+        n = n + 1;
         continue;// sai do loop while
 
     }
@@ -45,8 +45,8 @@ while (n <= 10)
     n++;
 }
 
-//Usando break e continue:
-for (int i = 1; i < 10; i++) 
+/*//Usando break e continue:
+for (int i = 1; i < 10; i++)
 {
     if (i == 5)
         continue;
@@ -56,7 +56,7 @@ for (int i = 1; i < 10; i++)
 
     Console.WriteLine(i);
 
-}
+}*/
 
 Console.WriteLine("\nFim do processamento...");
 Console.ReadKey();
